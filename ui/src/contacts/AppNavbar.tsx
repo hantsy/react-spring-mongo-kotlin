bb6859547066bc9da019f4e7b517c1f94ed33a4c
hantsy <hantsy@gmail.com>
2023-10-28 11:06:46 +0800

    chore: clean codes

--- ui/src/contacts/AppNavbar.tsx
+++ ui/src/contacts/AppNavbar.tsx
@@ -1,22 +1,37 @@
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
-import { Collapse, Nav, Navbar, NavbarBrand, NavbarToggler, NavItem, NavLink } from 'reactstrap';
+import {
+  Collapse,
+  Nav,
+  Navbar,
+  NavbarBrand,
+  NavbarToggler,
+  NavItem,
+  NavLink,
+} from 'reactstrap';
 
 const AppNavbar = () => {
-
   const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <Navbar color="dark" dark expand="md" className='mb-3'>
-      <NavbarBrand tag={Link} to="/">Address Book</NavbarBrand>
-      <NavbarToggler onClick={() => { setIsOpen(!isOpen) }}/>
+    <Navbar color="dark" dark expand="md" className="mb-3">
+      <NavbarBrand tag={Link} to="/">
+        Address Book
+      </NavbarBrand>
+      <NavbarToggler
+        onClick={() => {
+          setIsOpen(!isOpen);
+        }}
+      />
       <Collapse isOpen={isOpen} navbar>
-        <Nav className="justify-content-end" style={{width: "100%"}} navbar>
+        <Nav className="justify-content-end" style={{ width: '100%' }} navbar>
           <NavItem>
             <NavLink href="https://twitter.com/hantsy">@hantsy</NavLink>
           </NavItem>
           <NavItem>
-            <NavLink href="https://github.com/hantsy/react-spring-mongo-kotlin">GitHub</NavLink>
+            <NavLink href="https://github.com/hantsy/react-spring-mongo-kotlin">
+              GitHub
+            </NavLink>
           </NavItem>
         </Nav>
       </Collapse>
--- ui/src/contacts/ContactListPage.tsx
+++ ui/src/contacts/ContactListPage.tsx
@@ -1,58 +1,57 @@
-import { useEffect, useState } from "react";
-import { Link, useSearchParams } from "react-router-dom";
-import { Button } from "reactstrap";
-import { ContactList } from "./ContactList";
-import { ContactProvider, useContactContext } from "./ContactService";
-import { PersonSummary } from "./Model";
-import { PaginatedResult } from "./PaginatedResult";
-import { Paginator } from "./Paginator";
-import SearchBox from "./SearchBox";
+import { useEffect, useState } from 'react';
+import { Link, useSearchParams } from 'react-router-dom';
+import { Button } from 'reactstrap';
+import { ContactList } from './ContactList';
+import { ServiceProvider, useService } from './ContactService';
+import { PersonSummary } from './Model';
+import { PaginatedResult } from './PaginatedResult';
+import { Paginator } from './Paginator';
+import SearchBox from './SearchBox';
 
 const ContactListPage = () => {
   const [persons, setPersons] = useState<PersonSummary[]>([]);
   const [count, setCount] = useState<number>(0);
   const [loading, setLoading] = useState<boolean>(false);
   const [searchParams, setSearchParams] = useSearchParams();
-  const contactService = useContactContext();
+  const contactService = useService();
 
   useEffect(() => {
     setLoading(true);
     contactService
       .getAll(
-        searchParams.get("query"),
-        parseInt(searchParams.get("offset") ?? "0"),
-        parseInt(searchParams.get("limit") ?? "0")
+        searchParams.get('query'),
+        parseInt(searchParams.get('offset') ?? '0'),
+        parseInt(searchParams.get('limit') ?? '0')
       )
-      .then((res:PaginatedResult<PersonSummary>) => {
+      .then((res: PaginatedResult<PersonSummary>) => {
         setPersons(res.data);
-        setCount(res.count)
+        setCount(res.count);
         setLoading(false);
       });
   }, [contactService, searchParams]);
 
-
   const onQueryChange = (query: string) => {
     setSearchParams({
       q: query,
-      offset: "0",
-      limit: "10",
+      offset: '0',
+      limit: '10',
     });
   };
 
   const onPageChange = (offset: number) => {
-    setSearchParams({ ...searchParams.entries, offset: offset + "" });
+    setSearchParams({ ...searchParams.entries, offset: offset + '' });
   };
 
   if (loading) {
     return <p>Loading...</p>;
   }
   return (
-    <ContactProvider>
+    <ServiceProvider>
       <h2>Contact List</h2>
       <div className="row mt=5">
         <div className="col-md-6 md-12">
           <SearchBox
-            query={searchParams.get("q") ?? ""}
+            query={searchParams.get('q') ?? ''}
             handleSearch={onQueryChange}
           />
         </div>
@@ -69,12 +68,12 @@ const ContactListPage = () => {
       </div>
       <ContactList persons={persons} setPersons={setPersons} />
       <Paginator
-        offset={parseInt(searchParams.get("offset") ?? "0")}
-        limit={parseInt(searchParams.get("limit") ?? "10")}
+        offset={parseInt(searchParams.get('offset') ?? '0')}
+        limit={parseInt(searchParams.get('limit') ?? '10')}
         count={count}
         handlePageClick={onPageChange}
       />
-    </ContactProvider>
+    </ServiceProvider>
   );
 };
 
